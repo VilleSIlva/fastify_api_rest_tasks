799dfacb372b8b481a2065eceac49e23ef9ab488
VilleSilva <villenunescode@gmail.com>
2025-10-03 17:25:29 -0300

    Rotas para listar todas as transactions e uma unica transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,27 @@ import crypto from 'node:crypto'
 import z from 'zod'
 
 export async function transactionRoute(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+    const transaction = await knex('transactions')
+      .where('id', id)
+      .select('*')
+      .first()
+
+    return {
+      transaction,
+    }
+  })
+
   app.post('/', async (request, response) => {
     const createTransactionsBodySchema = z.object({
       title: z.string(),
